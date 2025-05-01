5c8c485ab40e528469d9ef567bfaffb60c1f3e6a
Alex PONDART <alex.pondart@oclock.school>
2025-05-01 23:59:16 +0200

    ProductTable

--- src/components/FilterableProductTable/ProductTable/ProductTable.jsx
+++ src/components/FilterableProductTable/ProductTable/ProductTable.jsx
@@ -1,11 +1,33 @@
 import ProductCategoryRow from "./ProductCategoryRow.jsx";
 import ProductRow from "./ProductRow.jsx";
 
-export default function ProductTable() {
+export default function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
   return (
-    <>
-      <ProductCategoryRow />
-      <ProductRow />
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
   );
 }
