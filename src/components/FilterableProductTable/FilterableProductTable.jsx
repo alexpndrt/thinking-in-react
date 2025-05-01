81733344c735d359036ba20e88dbffd4514fa7bb
Alex PONDART <alex.pondart@oclock.school>
2025-05-01 23:47:20 +0200

    structure components

---
+++ src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "./SearchBar/SearchBar";
+import ProductTable from "./ProductTable/ProductTable";
+
+export default function FilterableProductTable() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable />
+    </>
+  );
+}
