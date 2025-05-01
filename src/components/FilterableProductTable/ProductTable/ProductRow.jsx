559c260b5598220b6730b8b60b43bd91a0f3bb39
Alex PONDART <alex.pondart@oclock.school>
2025-05-01 23:54:00 +0200

    ProductRow

--- src/components/FilterableProductTable/ProductTable/ProductRow.jsx
+++ src/components/FilterableProductTable/ProductTable/ProductRow.jsx
@@ -1,1 +1,13 @@
-export default function ProductRow() {}
+export default function ProductRow({ product }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
