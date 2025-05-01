b3eee461e83dde68b986b4cff0dbc2d7ff3ba599
Alex PONDART <alex.pondart@oclock.school>
2025-05-01 23:59:54 +0200

    SearchBar

--- src/components/FilterableProductTable/SearchBar/SearchBar.jsx
+++ src/components/FilterableProductTable/SearchBar/SearchBar.jsx
@@ -1,3 +1,10 @@
 export default function SearchBar() {
-  return <></>;
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" /> Only show products in stock
+      </label>
+    </form>
+  );
 }
